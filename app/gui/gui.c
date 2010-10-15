a5d88fa072d67094594dfad9f4af4fbd98a0cf8d
Michael Natterer <mitch@gimp.org>
2010-10-15 09:56:27 +0200

    Switch to version 3.0 in a lot of places and depend on GTK+ 3.2
    and lots of other newer libraries.

--- app/gui/gui.c
+++ app/gui/gui.c
@@ -252,9 +252,9 @@ gui_init (Gimp     *gimp,
 static gchar *
 gui_sanity_check (void)
 {
-#define GTK_REQUIRED_MAJOR 2
-#define GTK_REQUIRED_MINOR 24
-#define GTK_REQUIRED_MICRO 10
+#define GTK_REQUIRED_MAJOR 3
+#define GTK_REQUIRED_MINOR 4
+#define GTK_REQUIRED_MICRO 0
 
   const gchar *mismatch = gtk_check_version (GTK_REQUIRED_MAJOR,
                                              GTK_REQUIRED_MINOR,
