137dd12537074114a233fd6d641f4e1ee53e01fb
Alexandre Prokoudine <alexandre.prokoudine@gmail.com>
2012-07-21 21:17:13 +0400

    Unkommented settings for preferring a dark GTK+ theme.
    All things work with two exceptions (so far): the ruler widget and 2nd level dialogs (e.g. PNG saving options) are not affected.

--- app/gui/themes.c
+++ app/gui/themes.c
@@ -87,11 +87,10 @@ themes_init (Gimp *gimp)
       g_free (path);
     }
 
-#if 0
+
   g_object_set (gtk_settings_get_for_screen (gdk_screen_get_default ()),
                 "gtk-application-prefer-dark-theme", TRUE,
                 NULL);
-#endif
 
   themes_style_provider = GTK_STYLE_PROVIDER (gtk_css_provider_new ());
 
